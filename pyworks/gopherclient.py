aa60e96952ffe9c437c9484e30ce799193d1fc23
chisper <wodemh2366@163.com>
2016-09-14 17:22:48 +0800

    update connect.py

--- pyworks/gopherclient.py
+++ pyworks/gopherclient.py
@@ -5,9 +5,13 @@ import socket,sys
 port = 70
 host = sys.argv[1]
 filename = sys.argv[2]
-
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((host,port))
+
+try:
+	s.connect((host,port))
+except socket.gaierror,e:
+	print "Error connecting to server : %s" % e
+	sys.exit(1)
 
 s.sendall(filename + "\r\n")
 
